458c8a30c7b9a421dde40a449d2c91c3cfbe525e
RichardRodri26 <richardmanuel811@gmail.com>
2026-09-23 23:00:08 -0500

    Sección 15: Composite

--- 02-estructurales/03-composite.ts
+++ 02-estructurales/03-composite.ts
@@ -13,3 +13,72 @@
  * https://refactoring.guru/es/design-patterns/composite
  *
  */
+
+interface FileSystemComponent {
+  showDetails(indent?: string): void;
+}
+
+class File implements FileSystemComponent {
+
+  private name: string;
+
+  constructor(name: string) {
+    this.name = name;
+  }
+
+  showDetails(indent?: string) {
+    console.log(`${indent} - ${this.name}`);
+  }
+
+}
+
+class Folder implements FileSystemComponent {
+
+  private name: string;
+  private contents: FileSystemComponent[] = [];
+
+  constructor(name: string) {
+    this.name = name;
+  }
+
+  add(component: FileSystemComponent): void {
+    this.contents.push(component);
+  }
+
+  showDetails(indent: string = '') {
+    console.log(`${indent}+ Carpeta: ${this.name}`);
+    this.contents.forEach(component => component.showDetails(indent + ' '));
+  }
+
+}
+
+function main() {
+  const file1 = new File('Archivo1.tsx');
+  const file2 = new File('Archivo2.tsx');
+  const file3 = new File('Archivo3.tsx');
+  const file4 = new File('Archivo4.tsx');
+
+  const folder1 = new Folder('Carpeta 1');
+  const folder5 = new Folder('Carpeta 5');
+
+  folder1.add(file1);
+  folder1.add(file2);
+
+  const folder2 = new Folder('Carpeta 2');
+  folder2.add(file3);
+
+  const folder3 = new Folder('Carpeta 3');
+  folder3.add(file4);
+  folder2.add(folder3);
+  folder2.add(folder5);
+
+  const rootFolder = new Folder('Carpeta ROOT');
+
+  rootFolder.add(folder1);
+  rootFolder.add(folder2);
+
+  rootFolder.showDetails();
+
+}
+
+main();
